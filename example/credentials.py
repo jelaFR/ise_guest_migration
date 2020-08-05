7448a9e491ed9a1b030997b3b049729c6e5470d8
Jean-Eudes LAYRISSE <je.layrisse@spf-solutions.fr>
2020-08-05 12:50:15 +0200

    Add credential.py example

---
+++ example/credentials.py
@@ -0,0 +1,10 @@
+# Define legacy ISE credentials
+LEGACY_ISE_URL = "https://192.168.1.1:9060"
+LEGACY_ISE_LOGIN = "user1_sponsort"
+LEGACY_ISE_PASSWORD = "user1_password"
+
+# Define new ISE credentials
+NEW_ISE_URL = "https://192.168.1.2:9060"
+NEW_ISE_LOGIN = "user2_sponsort"
+NEW_ISE_PASSWORD = "user2_password"
+NEW_ISE_PORTAL_ID = "123a45b6-7c89-10d1-11e2-1314151617f1"
